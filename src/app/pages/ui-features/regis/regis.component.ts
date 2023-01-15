cf5ef432c3baee1ee374cf95aff94403416b5f20
farmanhaider <farmanhaider2402gmail.com git config --global user.email farmanhaider2402gmail.com>
2023-01-15 19:48:57 +0530

    upload

---
+++ src/app/pages/ui-features/regis/regis.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import{FormGroup,FormControl,Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../service/user-auth.service';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'ngx-regis',
+  templateUrl: './regis.component.html',
+  styleUrls: ['./regis.component.scss']
+})
+export class RegisComponent {
+myForm=new FormGroup({
+  firstName:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$'),Validators.minLength(2),Validators.maxLength(20)]),
+  lastName:new FormControl('',[Validators.required,Validators.pattern('[a-zA-Z]+$'),Validators.minLength(2),Validators.maxLength(20)]),
+  email:new FormControl('',[Validators.required,Validators.pattern("[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*")]),
+  password:new FormControl('',[Validators.required,Validators.pattern('(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}')]),
+  contactNumber:new FormControl('',[Validators.required,Validators.pattern('[6-9][0-9]{9}')]),
+  // otp:new FormControl('',[Validators.required,Validators.pattern('[0-9]{ ,6}')]),
+})
+
+constructor (private userServ:UserAuthService,private router:Router){}
+get fdata(){
+  return this.myForm.controls;
+}
+  postData()
+  {
+    let formdata = this.myForm.getRawValue();
+    console.log(formdata)
+    this.userServ.postRegis(formdata).subscribe((res: any) => {
+     
+      console.log("res1:" + res.uid)
+    
+      if (res.err == 0) {
+        setInterval(() => {
+ this.router.navigate(['pages/ui-features/otp-page']).then(() => {
+            this.userServ.setData(res.uid);
+          })
+        })
+      }
+      if (res.err == 1) {
+        Swal.fire(`${res.msg}`,'','warning');
+      }
+    })
+  }
+
+}
